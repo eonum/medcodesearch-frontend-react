ad316d46be1191c7956274c690013231e1b2511c
JanKoch99 <jankoch99@bluewin.ch>
2022-03-09 15:30:37 +0100

    added fetching

--- medcodesearch-frontend-react/src/Components/Main/Main.js
+++ medcodesearch-frontend-react/src/Components/Main/Main.js
@@ -3,6 +3,25 @@ import './Main.css';
 
 class Main extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "",
+            code: ""
+        }
+    }
+
+
+    componentDidMount() {
+        fetch("https://search.eonum.ch/de/icd_chapters/ICD10-GM-2014/II")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    text: json.text
+                })
+            })
+    }
 
     render() {
         return (
@@ -11,7 +30,7 @@ class Main extends Component {
                     <div className="col-2"/>
                     <div className="col-8">
                         <div className="bg-secondary border border-5 border-bottom-0 border-top-0 border-right-0 border-end-0 border-primary rounded">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorem ea in obcaecati voluptates! A ab amet cumque dicta dolore, et ex minima nam perferendis rem repudiandae rerum, tempore vel. Accusamus amet autem blanditiis consequuntur culpa delectus deleniti dolore dolorum eius fugit hic ipsam iure laborum minus molestiae, molestias nulla officia optio pariatur quibusdam reiciendis reprehenderit sed similique sunt ut veniam veritatis voluptatibus. A dicta inventore laboriosam minus odit omnis quibusdam repellendus rerum tempore, voluptatibus! Atque, eligendi harum molestias nobis nulla recusandae? Aspernatur, at corporis, distinctio dolores dolorum iure magnam minima minus nulla pariatur perferendis quos recusandae sapiente, suscipit totam.
+                            {this.state.text}
                         </div>
                     </div>
                     <div className="col-2"/>
