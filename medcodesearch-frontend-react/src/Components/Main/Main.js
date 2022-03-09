e76e94b4dc458e99d915471029f7de173b47cd09
JanKoch99 <jankoch99@bluewin.ch>
2022-03-09 14:58:38 +0100

    added the main-body

---
+++ medcodesearch-frontend-react/src/Components/Main/Main.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import './Main.css';
+
+class Main extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-2"/>
+                    <div className="col-8">
+                        <div className="bg-secondary border border-5 border-bottom-0 border-top-0 border-right-0 border-end-0 border-primary rounded">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorem ea in obcaecati voluptates! A ab amet cumque dicta dolore, et ex minima nam perferendis rem repudiandae rerum, tempore vel. Accusamus amet autem blanditiis consequuntur culpa delectus deleniti dolore dolorum eius fugit hic ipsam iure laborum minus molestiae, molestias nulla officia optio pariatur quibusdam reiciendis reprehenderit sed similique sunt ut veniam veritatis voluptatibus. A dicta inventore laboriosam minus odit omnis quibusdam repellendus rerum tempore, voluptatibus! Atque, eligendi harum molestias nobis nulla recusandae? Aspernatur, at corporis, distinctio dolores dolorum iure magnam minima minus nulla pariatur perferendis quos recusandae sapiente, suscipit totam.
+                        </div>
+                    </div>
+                    <div className="col-2"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Main
