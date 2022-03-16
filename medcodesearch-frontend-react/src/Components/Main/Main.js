74859e7ceec40619c5ea762e3fe57097ffdcdbaa
JanKoch99 <jankoch99@bluewin.ch>
2022-03-16 22:52:29 +0100

    added flexible versions

--- medcodesearch-frontend-react/src/Components/Main/Main.js
+++ medcodesearch-frontend-react/src/Components/Main/Main.js
@@ -3,6 +3,14 @@ import './Main.css';
 import ICD from "../ICD/ICD";
 
 class Main extends Component {
+    version;
+
+
+    constructor(props) {
+        super(props);
+
+        this.version = `ICD10-GM-2022`
+    }
 
 
     render() {
@@ -13,10 +21,10 @@ class Main extends Component {
                         <div id="color" className="whiteBackground border border-5 border-bottom-0 border-top-0 border-right-0 border-end-0 rounded">
                             <div className="text-start ms-3">
                                 <h6 className="pb-4">ICD-10-GM-2022</h6>
-                                <h3>ICD-10-GM-2022</h3>
+                                <h3>{this.version}</h3>
                                 <h4>Untergeordnete Codes</h4>
                                 <ul>
-                                    <ICD version="ICD10-GM-2022"/>
+                                    <ICD version={this.version}/>
                                 </ul>
                             </div>
                         </div>
