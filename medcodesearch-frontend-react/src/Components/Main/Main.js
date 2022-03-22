6feb96cfc15cd0349a5193542e936270df45a75b
JanKoch99 <jankoch99@bluewin.ch>
2022-03-22 15:33:56 +0100

    fixed language bug

--- medcodesearch-frontend-react/src/Components/Main/Main.js
+++ medcodesearch-frontend-react/src/Components/Main/Main.js
@@ -29,8 +29,8 @@ class Main extends Component {
                         <div id="color" className="whiteBackground border border-5 border-bottom-0 border-top-0 border-right-0 border-end-0 rounded">
                             <div className="text-start ms-3">
                                 <h6 className="pb-4">ICD-10-GM-2022</h6>
-                                {/*<ICD version={this.version} category={this.buttons}/>*/}
-                                <ICDChild version={this.version} category={this.buttons} page={this.page} language={this.language}/>
+                                <ICD version={this.version} category={this.buttons} language={this.language}/>
+                                {/*<ICDChild version={this.version} category={this.buttons} page={this.page} language={this.language}/>*/}
                             </div>
                         </div>
                     </div>
