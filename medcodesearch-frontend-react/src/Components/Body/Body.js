4c06997f7afabae68ec1dd3b9060961650a30f43
Kevin Stöckli <82031534+ShutCoffee@users.noreply.github.com>
2022-03-30 21:16:07 +0200

    Revert "merged body and bodyChild"
    This reverts commit ed9a3b365c87c2de2b3dd885bddab876494ac330.

--- medcodesearch-frontend-react/src/Components/Body/Body.js
+++ medcodesearch-frontend-react/src/Components/Body/Body.js
@@ -8,32 +8,23 @@ class Body extends Component{
         super(props);
         this.state = {
             children:[],
-            exclusions: [],
-            inclusions: []
         }
     }
 
     componentDidMount() {
-        if (this.props.page === undefined) {
-            this.fetchInformationsMain()
-        } else {
-            this.fetchInformationsChild()
-        }
+        this.fetchInformations()
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(prevProps.language !== this.props.language ||
             prevProps.version !== this.props.version ||
             prevProps.category !== this.props.category) {
-            if (this.props.page === undefined) {
-                this.fetchInformationsMain()
-            } else {
-                this.fetchInformationsChild()
-            }
+            this.fetchInformations()
         }
     }
 
-    async fetchInformationsMain() {
+    async fetchInformations() {
+        console.log(this.props.version)
         fetch(`https://search.eonum.ch/`+this.props.language+`/`+this.props.category+`/`+this.props.version+`/`+this.props.version+`?show_detail=1`)
             .then((res) => res.json())
             .then((json) => {
@@ -48,80 +39,16 @@ class Body extends Component{
             })
     }
 
-    async fetchInformationsChild() {
-        fetch(`https://search.eonum.ch/`+this.language+`/`+this.category+`/`+this.version+`/`+this.page+`?show_detail=1`)
-            .then((res) => res.json())
-            .then((json) => {
-                this.setState({
-                    children: json.children,
-                    exclusions: json.exclusions,
-                    inclusions: json.inclusions
-                })
-            })
-    }
-
-    lookingForLink(aString) {
-        var splitStr = aString.split(` {`)
-        if (splitStr.length > 1){
-            var endString = splitStr[1].split(`}`)
-            return (
-                <li className="Exclusion" key={aString}>{splitStr[0]} (<a href="">{endString[0]}</a>)</li>
-            )
-        } else {
-            return splitStr
-        }
-    }
-
     render() {
         return (
             <div>
-                {this.props.page === undefined && (
-                    <div>
-                        <h3>{this.props.version}</h3>
-                        <h4>Untergeordnete Codes</h4>
-                        <ul>
-                            {this.state.children.map((child) => (
-                                <li className="ICD" key={child.code}><a className="link" href="">{child.code}:</a> {child.text}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-                {this.props.page !== undefined && (
-                    <div>
-                        <h4>{this.page}</h4>
-
-                        {this.state.exclusions.length > 0 && (
-                            <div>
-                                <h5>Exklusionen</h5>
-
-                                <ul>
-                                    {this.state.exclusions.map((exclusion) => (
-                                        this.lookingForLink(exclusion)
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-
-                        {this.state.inclusions.length > 0 && (
-                            <div>
-                                <h5>Inklusionen</h5>
-
-                                <ul>
-                                    {this.state.inclusions.map((inclusion) => (
-                                        <li className="Inclusion" key={inclusion}>{inclusion}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-
-                        <h5>Untergeordnete Codes</h5>
-                        <ul>
-                            {this.state.children.map((child) => (
-                                <li className="ICD" key={child.code}><a href="">{child.code}:</a> {child.text}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                <h3>{this.props.version}</h3>
+                <h4>Untergeordnete Codes</h4>
+                <ul>
+                    {this.state.children.map((child) => (
+                        <li className="ICD" key={child.code}><a className="link" href="">{child.code}:</a> {child.text}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
