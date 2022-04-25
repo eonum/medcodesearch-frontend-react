443e4e715de8e9d36bc0668389b869965e688022
4l3s5i0Z <alessio.petrone@students.unibe.ch>
2022-04-25 19:54:46 +0200

    update

--- medcodesearch-frontend-react/src/setupTests.js
+++ medcodesearch-frontend-react/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+const {Builder} = require('selenium-webdriver');
+require('selenium-webdriver/chrome')
+require('selenium-webdriver/firefox')
+require('chromedriver')
+require('geckodriver')
+jest.setTimeout(30000)
