daed40838060ad2252c58c7056d8bdd9ef7739d1
JanKoch99 <jankoch99@bluewin.ch>
2022-05-17 23:46:30 +0200

    added JSDoc to Services

--- medcodesearch-frontend-react/src/Services/router.service.js
+++ medcodesearch-frontend-react/src/Services/router.service.js
@@ -1,8 +1,15 @@
 import {Component} from "react";
 
+/**
+ * Responsible for the url route
+ */
 class RouterService extends Component {
 
-
+    /**
+     * get the query variable and compares it
+     * @param variable
+     * @returns {string}
+     */
     static getQueryVariable(variable) {
         let query = window.location.search.substring(1);
         let vars = query.split('&');
@@ -15,18 +22,32 @@ class RouterService extends Component {
         return "";
     }
 
+    /**
+     * get the language defined in the url and if it isn't defined set it to 'de'
+     * @returns {string|*}
+     */
     static getLanguageFromURL() {
         if(window.location.pathname !== '/') {
             return window.location.pathname.split("/")[1]
         }
         return 'de'
     }
+
+    /**
+     * get the category defined in the url and if it isn't defined set it to 'ICD'
+     * @returns {string|*}
+     */
     static getCategoryFromURL() {
         if(window.location.pathname !== '/') {
             return window.location.pathname.split("/")[2]
         }
         return 'ICD'
     }
+
+    /**
+     * get the version defined in the url and if it isn't defined set it to 'ICD10-GM-2022'
+     * @returns {string|*}
+     */
     static getVersionFromURL() {
         if(window.location.pathname !== '/') {
             return window.location.pathname.split("/")[3]
