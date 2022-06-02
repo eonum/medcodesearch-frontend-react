c492376bf813334a38016752c2b9ac30b43f40bb
Tim Peter <tim.peter@eonum.ch>
2022-06-02 08:21:03 +0200

    Change links to eonum website.

--- medcodesearch-frontend-react/src/Components/Footer/footer.js
+++ medcodesearch-frontend-react/src/Components/Footer/footer.js
@@ -15,7 +15,7 @@ class Footer extends Component {
         return (
             <footer className='footer'> 
                 <div className='footer-text'>
-                    <p>copyright by <a className="link" href='http://eonum.ch' target='_blank' rel='noreferrer'>eonum</a></p>
+                    <p>copyright by <a className="link" href='https://eonum.ch/de/blog/medcodesearch-ergaenzung-des-suchdienstes-mit-migel-analysenliste-al-sowie-medikamente-gtin/' target='_blank' rel='noreferrer'>eonum</a> 2022 | <a className="link" href='https://eonum.ch/de/kontakt/' target='_blank' rel='noreferrer'>contact</a></p>
                 </div> 
             </footer>
         );
