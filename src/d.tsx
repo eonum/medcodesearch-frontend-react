51fe54ca6164a6f8cc82ae470b6ca18a35e2e5c2
Joel Bessire <joel.bessire@eonum.ch>
2022-06-24 13:50:12 +0200

    adapt tsconfig and add d.tsx for imports

---
+++ src/d.tsx
@@ -0,0 +1,12 @@
+declare module "*.png" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    import React = require('react');
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
