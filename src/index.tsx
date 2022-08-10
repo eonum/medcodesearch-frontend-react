2a9f6ed4aa08653e710c5634d4d565ef321b94fb
Joel Bessire <joel.bessire@eonum.ch>
2022-08-10 13:32:47 +0200

    revert 3d9fb81 since this is update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
@@ -8,13 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 /**
  * Renders the whole application
  */
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// TODO: Use "createRoot" instead of "ReactDOM.render".
+//  This will enable React 18, but also leads to failed tests. Thus this maybe needs some more time...
+ReactDOM.render(
     // StrictMode renders components twice (on dev but not production), detecting problems / deprecations / warnings.
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
+    document.getElementById('root')
 );
