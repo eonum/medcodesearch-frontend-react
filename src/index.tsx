3d9fb81564b52fa5362768d4039ebc6c997134cb
Joel Bessire <joel.bessire@eonum.ch>
2022-08-10 09:39:17 +0200

    use createRoot instead of deprecated ReactDom.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
@@ -11,12 +11,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // TODO: render app here and move <Router /> part to app
 //  (see casematch or https://github.com/eonum/medlandscape/blob/master/src/index.js for examples)
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     // StrictMode renders components twice (on dev but not production), detecting problems / deprecations / warnings.
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
-  document.getElementById('root')
 );
