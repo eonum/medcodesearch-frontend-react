a7369876be7c1e4a4a6faa08cb63c6d519f9769c
Joel Bessire <joel.bessire@eonum.ch>
2022-08-10 09:40:07 +0200

    removed solved TODO

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  * Renders the whole application
  */
 
-// TODO: render app here and move <Router /> part to app
-//  (see casematch or https://github.com/eonum/medlandscape/blob/master/src/index.js for examples)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // StrictMode renders components twice (on dev but not production), detecting problems / deprecations / warnings.
