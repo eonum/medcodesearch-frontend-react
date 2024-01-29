95543c44b12605ad050a8dbc1c785e3e96089e3b
Joel Bessire <joel.bessire@eonum.ch>
2024-01-29 08:43:04 +0100

    corrected accidentally committed localhost URL

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -59,7 +59,7 @@ export const versionsWithoutMappingInfos = [
     "CHOP 2014",
     "CHOP 2015"]
 
-export const fetchURL = 'http://localhost:3001/'
+export const fetchURL = 'https://search.eonum.ch/'
 
 // Get frontend url for navigation from backend code url.
 export function getNavParams(code, language, catalog, resource_type?) {
