1b7aef2c74011a3817c95edd34fdb4ef229c32c0
Joel Bessire <joel.bessire@eonum.ch>
2025-02-17 15:22:22 +0100

    refactoring footer component into functional

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './footer.css';
 
 /**
- * is the footer of the website and shows the copyright by eonum
+ * footer of the website, shows the copyright by eonum
  * @component
  */
-class Footer extends Component {
+const Footer: React.FC = () => {
 
     /**
      * renders the footer
      * @returns {JSX.Element}
      */
-    render() { 
-        return (
-            <footer className='footer'> 
-                <div className='footer-text'>
-                    <p>copyright by <a className="link" href='https://eonum.ch/de/blog/medcodesearch-ergaenzung-des-suchdienstes-mit-migel-analysenliste-al-sowie-medikamente-gtin/' target='_blank' rel='noreferrer'>eonum</a> 2022 | <a className="link" href='https://eonum.ch/de/kontakt/' target='_blank' rel='noreferrer'>contact</a></p>
-                </div> 
-            </footer>
-        );
-    }
+    return (
+        <footer className='footer'>
+            <div className='footer-text'>
+                <p>
+                    copyright by <a
+                    className="link"
+                    href='https://eonum.ch'
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    eonum
+                </a> 2024 | <a
+                    className="link"
+                    href='https://eonum.ch/de/kontakt/'
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    contact
+                </a>
+                </p>
+            </div>
+        </footer>
+    );
 }
- 
+
 export default Footer;
