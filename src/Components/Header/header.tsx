e316a578a990bf2ab674d74c54a1d9b8e877ca80
Joel Bessire <joel.bessire@eonum.ch>
2025-02-17 15:17:11 +0100

    refactoring header component into functional

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './header.css'
 import {IUpdateStateByArg} from "../../interfaces";
 import {useTranslation} from "react-i18next";
@@ -6,81 +6,37 @@ import {useTranslation} from "react-i18next";
 interface Props {
     changeLanguage: IUpdateStateByArg,
     activeLanguage: string,
-    translation: any
-}
-
-interface IHeader {
-    languagePrev: string,
-    language: string
 }
 
 /**
- * is the header of the website, which is responsible for language changes
+ * header of the website, responsible for language changes
  * @component
  */
-class Header extends Component<Props, IHeader> {
+const Header: React.FC<Props> = ({ changeLanguage, activeLanguage }) => {
+    const { i18n, t } = useTranslation();
+    const buttons = ['de', 'fr', 'it', 'en']
 
-    buttons = ['de', 'fr', 'it', 'en']
+    const handleLanguageChange = (lang: string) => {
+        changeLanguage(lang);
+        i18n.changeLanguage(lang);
+    };
 
-    /**
-     * Constructor sets the default language and calls the updateLanguage to look for changes
-     * @param props
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            languagePrev: 'de',
-            language: 'de'
-        };
-        this.updateLanguage = this.updateLanguage.bind(this);
-    }
-
-    /**
-     * Change the languagePrev state to the current language
-     * @param lang
-     */
-    updateLanguage(lang) {
-        const {i18n} = this.props.translation;
-
-        this.props.changeLanguage(lang)
-        this.setState({languagePrev: this.state.language, language: lang})
-        i18n.changeLanguage(lang)
-    }
-
-    /**
-     * If the component mounted componentDidMount() looks for a change in the language
-     */
-    componentDidMount() {
-        if(this.state.language !== this.props.activeLanguage){
-            this.updateLanguage(this.props.activeLanguage)
-        }
-    }
-
-    /**
-     * render full header for the website
-     * @returns {JSX.Element}
-     */
-    render() {
-        return (
-                <header className='header'>
-                    <div className='language-selection'>
-                        {this.buttons.map(buttonLabel =>
-                            <button onClick={this.updateLanguage.bind(null, buttonLabel)}
-                                    key={buttonLabel}
-                                    className= "language-btn"
-                                    id={buttonLabel === this.props.activeLanguage ? "activeLan" : ""}
-                                    >
-                               {buttonLabel}
-                            </button>)}
-                    </div>
-                </header>
-        );
-    }
+    return (
+        <header className='header'>
+            <div className='language-selection'>
+                {buttons.map(buttonLabel => (
+                    <button
+                        onClick={() => handleLanguageChange(buttonLabel)}
+                        key={buttonLabel}
+                        className="language-btn"
+                        id={buttonLabel === activeLanguage ? "activeLan" : ""}
+                    >
+                        {buttonLabel}
+                    </button>
+                ))}
+            </div>
+        </header>
+    );
 }
 
-
-function addProps(Component) {
-    return props => <Component {...props} translation={useTranslation()}/>;
-}
-
-export default addProps(Header);
+export default Header;
